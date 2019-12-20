483e3a45c76b3c2c576a5a17983edb1678a70db1
wymerkd <wymerkd@gmail.com>
2019-12-20 10:01:50 -0800

    add html views to account for user input

--- app.rb
+++ app.rb
@@ -3,3 +3,34 @@ require('sinatra/reloader')
 require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  if params["search"]
+    @words = Word.search(params[:search])
+  else
+    @words = Word.all
+  end
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  word_input = params[:user_word]
+  word = Word.new(nil, word_input)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
