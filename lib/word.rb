deede836fcf991247dfe6d6ecd06357c8d14f395
wymerkd <wymerkd@gmail.com>
2019-12-20 09:08:31 -0800

    pass test for '#=='

--- lib/word.rb
+++ lib/word.rb
@@ -8,9 +8,8 @@ class Word
     @id = id || @@total_rows += 1
     @word_input = word_input
   end
-end
 
-#   def save
-#     @@words[self.id] = Word.new(self.id, self.word_input)
-#   end
-# end
+  def ==(word_to_compare)
+    self.word_input() == word_to_compare.word_input()
+  end
+end
