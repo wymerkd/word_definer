1c8117bdaeb1ccd0514a8c52e96fb9b495b2ed29
wymerkd <wymerkd@gmail.com>
2019-12-20 08:56:49 -0800

    fail test for #save spec

--- lib/word.rb
+++ lib/word.rb
@@ -1,1 +1,11 @@
 class Word
+  attr_accessor :id, :word
+
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(id, word)
+    @id = id || @@total_rows += 1
+    @name = name
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,2 +1,19 @@
 require 'rspec'
 require 'word'
+
+describe '#Word' do
+
+  before(:each) do
+    Word.clear()
+  end
+end
+
+describe('#save') do
+    it("saves a word") do
+      word = Word.new(nil, "bumfuzzle") # nil, nil added as second argument
+      album.save()
+      album2 = Word.new(nil, "cattywampus" ) # nil, nil added as second argument
+      word2.save()
+      expect(Word.all).to(eq([album, album2]))
+    end
+  end
