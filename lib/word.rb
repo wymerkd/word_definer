929530bd34ecefcb64eb3fdd31aaa8a88a350b3b
wymerkd <wymerkd@gmail.com>
2019-12-20 10:22:52 -0800

    fail test for .clear

--- lib/word.rb
+++ lib/word.rb
@@ -9,6 +9,10 @@ class Word
     @word_input = word_input
   end
 
+  def save
+    @@words[self.id] = Word.new(self.id, self.word_input)
+  end
+
   def ==(word_to_compare) #comparing if two words are the same
     self.word_input() == word_to_compare.word_input()
   end
@@ -17,11 +21,12 @@ class Word
     @@words.values()
   end
 
-  def save
-    @@words[self.id] = Word.new(self.id, self.word_input)
-  end
+  # def self.clear
+  #   @@total_rows = 0
+  #   @@words = {}
+  # end
 
-  def delete
-    @@words.delete(self.id)
-  end
+  # def delete
+  #   @@words.delete(self.id)
+  # end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,50 +6,50 @@ describe '#Word' do
   before(:each) do
     Word.clear()
   end
-end
 
-describe('#==') do
-  it("is the same word if it has the same attributes as another word") do
-    word = Word.new(nil, "cattywampus")
-    word2 = Word.new(nil, "cattywampus")
-    expect(word).to(eq(word2))
+  describe('#==') do
+    it("is the same word if it has the same attributes as another word") do
+      word = Word.new(nil, "cattywampus")
+      word2 = Word.new(nil, "cattywampus")
+      expect(word).to(eq(word2))
+    end
   end
-end
 
-describe('.all') do
-  it("returns an empty array when there are no words") do
-    expect(Word.all).to(eq([]))
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new(nil, "bumfuzzle")
+      word.save()
+      word2 = Word.new(nil, "cattywampus")
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
   end
-end
 
-describe('.clear') do
-  it("clears all albums") do
-    word = Word.new(nil, "bumfuzzle")
-    word.save()
-    word2 = Word.new(nil, "cattywampus")
-    word2.save()
-    expect(Word.all).to(eq([word, word2]))
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
   end
-end
 
-describe('#save') do
-  it("saves a word") do
-    word = Word.new(nil, "bumfuzzle")
-    word.save()
-    word2 = Word.new(nil, "cattywampus")
-    word2.save()
-    expect(Word.all).to(eq([word, word2]))
+  describe('.clear') do
+    it("clears all albums") do
+      word = Word.new(nil, "bumfuzzle")
+      word.save()
+      word2 = Word.new(nil, "cattywampus")
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
   end
+
+  # describe('#delete') do
+  #   it("deletes a word by id") do
+  #     word = Word.new(nil, "bumfuzzle")
+  #     word.save()
+  #     word2 = Word.new(nil, "cattywampus")
+  #     word2.save()
+  #     word.delete()
+  #     expect(Word.all).to(eq([word2]))
+  #   end
+  # end
 end
-
-
-# describe('#delete') do
-#   it("deletes a word by id") do
-#     word = Word.new(nil, "bumfuzzle")
-#     word.save()
-#     word2 = Word.new(nil, "cattywampus")
-#     word2.save()
-#     word.delete()
-#     expect(Word.all).to(eq([word2]))
-#   end
-# end
