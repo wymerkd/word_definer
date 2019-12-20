e1b54bcc25d321834f4bc2d8027f3442e3d8110e
wymerkd <wymerkd@gmail.com>
2019-12-20 12:31:13 -0800

    add patch to app.rb to update words

--- app.rb
+++ app.rb
@@ -34,3 +34,15 @@ get('/words/:id') do
   @word = Word.find(params[:id].to_i())
   erb(:word)
 end
+
+get('/words/:id/edit') do
+@word = Word.find(params[:id].to_i())
+erb(:edit_word)
+end
+
+patch('/words/:id') do
+@word  = Word.find(params[:id].to_i())
+@word.update(params[:user_word])
+@words = Word.all
+erb(:words)
+end
