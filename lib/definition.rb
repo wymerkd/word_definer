2a86b1dc5825fb7e170f1691ef53f31b98bda5bf
wymerkd <wymerkd@gmail.com>
2020-01-05 18:18:38 -0800

    pass test for definition's #update

--- lib/definition.rb
+++ lib/definition.rb
@@ -32,9 +32,9 @@ class Definition
     @@definitions[id]
   end
 
-  # def update(name, word_id)
-  #   self.name = name
-  #   self.word_id = word_id
-  #   @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
-  # end
+  def update(name, word_id)
+    self.name = name
+    self.word_id = word_id
+    @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
+  end
 end
