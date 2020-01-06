34f2c52011bd04f9f6d9bf468e10eccd7e467e9d
wymerkd <wymerkd@gmail.com>
2020-01-05 18:21:05 -0800

    pass test for definition's #delete

--- lib/definition.rb
+++ lib/definition.rb
@@ -38,7 +38,7 @@ class Definition
     @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
   end
 
-  # def delete
-  #   @@definitions.delete(self.id)
-  # end
+  def delete
+    @@definitions.delete(self.id)
+  end
 end
