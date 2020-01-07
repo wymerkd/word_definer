82e8ff78270b8153990a3d4d2f5167f26d1e7cc5
Student <example@example.com>
2020-01-07 06:48:20 -0800

    add specific id to word_integration spec for creating a definition path.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -9,17 +9,16 @@ describe('create a word path', {:type => :feature}) do
     click_on('Add a new word')
     fill_in('user_word', :with => 'cattywampus')
     click_on('Add!')
-    save_and_open_page
     expect(page).to have_content('cattywampus')
   end
 end
 
 describe('create a definition path', {:type => :feature}) do
   it('creates a definition and then goes to the definition page') do
-    word = Word.new("cattywampus", nil)
+    word = Word.new("cattywampus", 1)
     word.save
-    visit("/words/#{word.id}")
-    # save_and_open_page
+    visit("/words/1")
+    save_and_open_page
     fill_in('user_definition', :with => "not lined up evenly on each side")
     click_on('Add definition')
     expect(page).to have_content("not lined up evenly on each side")
